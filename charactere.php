87e6d9b8144811e6224067879efb18fd280e39cb
khaledze <k.aliahmed@ecole-ipssi.net>
2023-11-23 15:09:12 +0100

    drg

---
+++ charactere.php
@@ -0,0 +1,5 @@
+<?php
+$character_id = null; // Déclarez la variable pour stocker l'ID du personnage sélectionné
+$monster_id = null; // Déclarez la variable pour stocker l'ID du monstre
+// ... autres variables ou configurations spécifiques au personnage
+?>
