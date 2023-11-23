2818eabf596e8c0e8fd3d48c3da8d1bea2f86b79
khaledze <k.aliahmed@ecole-ipssi.net>
2023-11-23 15:16:22 +0100

    push

--- systeme.php
+++ systeme.php
@@ -9,12 +9,29 @@ class systeme{
         $this->connexion = $connexion;
     }
 
-function select_character() {
-    global $character_id;
-    // Logique pour permettre à l'utilisateur de sélectionner un personnage
-    // Peut-être une requête SQL pour afficher les personnages disponibles et choisir
-    // Mettre à jour $character_id avec l'ID du personnage sélectionné
-}
+    function select_character() {
+        global $character_id, $connexion;
+    
+        // Fetch all characters from the database
+        $stmt = $connexion->prepare("SELECT * FROM characters");
+        $stmt->execute();
+        $characters = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+        // Display all characters and let the user select one
+        echo "Please select a character:\n";
+        foreach ($characters as $index => $character) {
+            echo ($index + 1) . ". " . $character['name'] . "\n";
+        }
+    
+        $choice = trim(fgets(STDIN)) - 1;
+        if (isset($characters[$choice])) {
+            $character_id = $characters[$choice]['id'];
+            echo "You have selected " . $characters[$choice]['name'] . ".\n";
+        } else {
+            echo "Invalid choice. Please try again.\n";
+            $this->select_character();
+        }
+    }
 
 function start_game() {
     global $character_id, $monster_id;
@@ -46,12 +63,12 @@ $systeme = new Systeme($connexion);
 
 // Example of game flow (very simplified)
 $systeme->select_character();
-$systeme->start_game();
-$systeme->combat();
-$systeme->loot();
-$systeme->save_progress();
+// $systeme->start_game();
+// $systeme->combat();
+// $systeme->loot();
+// $systeme->save_progress();
 
-$connexion = null; // Close the database connection
+// $connexion = null; // Close the database connection
 ?>
 
 
