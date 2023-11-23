ae62ebfd9c8d8c5802ff3d14ee2c7b7faf4f3771
stiles667 <i.elyamani@ecole-ipssi.net>
2023-11-23 16:09:19 +0100

    Update systeme.php

--- systeme.php
+++ systeme.php
@@ -69,9 +69,61 @@ function start_game() {
 }
 function combat() {
     global $character_id, $monster_id;
-    // Logique pour gérer le combat avec le monstre
-    // Attaque, défense, esquive, etc.
-    // Mettre à jour les points d'expérience du personnage en cas de victoire
+
+    // Fetch character data
+    $stmt = $this->connexion->prepare("SELECT * FROM characters WHERE id = ?");
+    $stmt->execute([$character_id]);
+    $character = $stmt->fetch();
+
+    // Fetch a random monster
+    $stmt = $this->connexion->prepare("SELECT * FROM monsters ORDER BY RAND() LIMIT 1");
+    $stmt->execute();
+    $monster = $stmt->fetch();
+
+    if ($monster) {
+        // Loop until someone dies
+        while ($character['hp'] > 0 && $monster['hp'] > 0) {
+            // Get user action
+            $action = readline("Choose an action (1 = attack, 2 = dodge, 3 = defend): ");
+
+            switch ($action) {
+                case 1: // Attack
+                    if ($character['ap'] > $monster['dp']) {
+                        $monster['hp'] -= $character['ap'];
+                        echo "{$character['name']} attacks and hits the {$monster['name']}! Monster HP: {$monster['hp']}";
+                    } else {
+                        echo "{$character['name']} attacks but is blocked by the {$monster['name']}!";
+                    }
+                    break;
+                case 2: // Dodge
+                    echo "{$character['name']} dodges the attack of the {$monster['name']}!";
+                    break;
+                case 3: // Defend
+                    if ($monster['ap'] > $character['dp']) {
+                        $character['hp'] -= $monster['ap'];
+                        echo "{$character['name']} tries to defend but is hit by the {$monster['name']}! Character HP: {$character['hp']}";
+                    } else {
+                        echo "{$character['name']} defends successfully against the attack of the {$monster['name']}!";
+                    }
+                    break;
+            }
+
+            // Monster attacks
+            if ($monster['hp'] > 0) {
+                $character['hp'] -= $monster['ap'];
+                echo "The {$monster['name']} attacks! Character HP: {$character['hp']}";
+            }
+        }
+
+        // Check who died
+        if ($character['hp'] <= 0) {
+            echo "{$character['name']} is defeated by the {$monster['name']}!";
+        } else {
+            echo "{$character['name']} defeats the {$monster['name']}!";
+        }
+    } else {
+        echo "{$character['name']} finds no monsters to fight.";
+    }
 }
 
 function loot() {
@@ -93,7 +145,7 @@ $systeme = new Systeme($connexion);
 $systeme->select_character();
 $systeme->start_game();
 
-// $systeme->combat();
+$systeme->combat();
 // $systeme->loot();
 // $systeme->save_progress();
 
