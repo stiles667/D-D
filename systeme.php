9c57a7e2073a4ba4ceb66bc05ab4f42550bf152a
stiles667 <i.elyamani@ecole-ipssi.net>
2023-11-24 16:25:53 +0100

    ez

--- systeme.php
+++ systeme.php
@@ -35,7 +35,6 @@ class systeme{
     }
 
     function start_game() {
-    
         // Fetch character data
         $stmt = $this->connexion->prepare("SELECT * FROM characters WHERE id = ?");
         $stmt->execute([$this->character->id]);
@@ -47,8 +46,8 @@ class systeme{
     
         while (true) {
             // Fetch a random room
-            $stmt = $this->connexion->prepare("SELECT * FROM rooms WHERE merchant IS NULL ORDER BY RAND() LIMIT 1");
-            $stmt->execute();
+            $stmt = $this->connexion->prepare("SELECT * FROM rooms WHERE puzzle = 1 OR merchant IS NULL ORDER BY RAND() LIMIT 1");     
+                   $stmt->execute();
             $room = $stmt->fetch();
     
             if ($room) {
@@ -57,48 +56,49 @@ class systeme{
     
                 // Check if the room has a puzzle
                 if ($room['puzzle']) {
-                    // Fetch all puzzles
-                    $stmt = $this->connexion->prepare("SELECT id, question, choice1, choice2, choice3, answer FROM puzzles");
+                    // Fetch a random puzzle
+                    $stmt = $this->connexion->prepare("SELECT * FROM puzzles ORDER BY RAND() LIMIT 1");
                     $stmt->execute();
-                    $puzzles = $stmt->fetchAll();
-                
-                    // Select a random puzzle
-                    $puzzle = $puzzles[array_rand($puzzles)];
+                    $puzzle = $stmt->fetch();
                 
                     if ($puzzle) {
+                        // Display puzzle information
                         echo "This room has a puzzle: {$puzzle['question']}\n";
                         echo "1: {$puzzle['choice1']}\n";
                         echo "2: {$puzzle['choice2']}\n";
                         echo "3: {$puzzle['choice3']}\n";
-                
-                        $answer = readline("Enter your answer (1, 2, or 3): ");
-                
-                        if ($answer != $puzzle['answer']) {
-                            echo "Wrong answer! You lose points.\n";
-                            $character['hp'] -= 10; // Subtract 10 from HP
-
-                            // Update the character's HP in the database
-                            $stmt = $this->connexion->prepare("UPDATE characters SET hp = ? WHERE id = ?");
-                            $stmt->execute([$character['hp'], $character['id']]);
-                        } else {
+    
+                        // Get user input for the answer
+                        $answer = trim(fgets(STDIN));
+    
+                        // Check if the answer is correct
+                        if ($answer == $puzzle['answer']) {
                             echo "Correct answer! You gain points.\n";
                             // Add points to a random attribute
                             $attribute = array_rand(['hp' => 'hp', 'ap' => 'ap', 'dp' => 'dp']);
                             $character[$attribute] += 10;
-
+    
                             // Update the character's attribute in the database
                             $stmt = $this->connexion->prepare("UPDATE characters SET $attribute = ? WHERE id = ?");
                             $stmt->execute([$character[$attribute], $character['id']]);
-
+    
                             // Check if the character's HP is 0 or less
                             if ($character['hp'] <= 0) {
                                 echo "{$character['name']} has died.";
                                 break;
                             }
+                        } else {
+                            echo "Wrong answer! You lose points.\n";
+                            $character['hp'] -= 10; // Subtract 10 from HP
+    
+                            // Update the character's HP in the database
+                            $stmt = $this->connexion->prepare("UPDATE characters SET hp = ? WHERE id = ?");
+                            $stmt->execute([$character['hp'], $character['id']]);
                         }
                     } else {
                         echo "No puzzle found in this room.\n";
                     }
+                }
                 }
                 if ($room['trap']) {
                     echo "This room has a trap: {$room['trap']}\n";
@@ -120,6 +120,7 @@ class systeme{
                     // Call the merchant function
                     $this->merchant();
                 }
+                
     
                 // Initiate a combat if the room has no merchant and no puzzle
                 if (!$room['merchant'] && !$room['puzzle'] && !$room['trap']) {
@@ -132,7 +133,7 @@ class systeme{
                         // Après avoir vaincu le monstre, effectuez le loot
                         $this->loot();
                     }
-                }
+                
             } else {
                 echo "{$character['name']} enters a room but it's empty. They can rest for a while.";
             }
@@ -169,29 +170,31 @@ class systeme{
             }
         }
     }
-  function combat() {
-    // Fetch character data
-    $stmt = $this->connexion->prepare("SELECT * FROM characters WHERE id = ?");
-    $stmt->execute([$this->character->id]);
-    $character = $stmt->fetch();
-
-    // Save initial stats
-    $initialStats = [
-        'hp' => $character['hp'],
-        'ap' => $character['ap'],
-        'dp' => $character['dp']
-    ];
-
-    // Fetch a random monster
-    $stmt = $this->connexion->prepare("SELECT * FROM monsters ORDER BY RAND() LIMIT 1");
-    $stmt->execute();
-    $monster = $stmt->fetch();
-
-    if ($monster) {
-        echo "A {$monster['name']} appears and attacks you!";
-
-        // Loop until someone dies
-        while ($character['hp'] > 0 && $monster['hp'] > 0) {
+    function combat() {
+        // Fetch character data
+        $stmt = $this->connexion->prepare("SELECT * FROM characters WHERE id = ?");
+        $stmt->execute([$this->character->id]);
+        $character = $stmt->fetch();
+    
+        // Save initial stats
+        $initialStats = [
+            'hp' => isset($character['hp']) ? $character['hp'] : 0,
+            'ap' => isset($character['ap']) ? $character['ap'] : 0,
+            'dp' => isset($character['dp']) ? $character['dp'] : 0,
+            'experience' => isset($character['experience']) ? $character['experience'] : 0
+        ];
+        
+    
+        // Fetch a random monster
+        $stmt = $this->connexion->prepare("SELECT * FROM monsters ORDER BY RAND() LIMIT 1");
+        $stmt->execute();
+        $monster = $stmt->fetch();
+    
+        if ($monster) {
+            echo "A {$monster['name']} appears and attacks you!";
+    
+            // Loop until someone dies
+            while ($character['hp'] > 0 && $monster['hp'] > 0) {
             // Get user action
             $action = readline("Choose an action (1 = attack, 2 = dodge, 3 = defend): ");
 
@@ -217,51 +220,71 @@ class systeme{
                     break;
             }
 
-            // Monster attacks
-            if ($monster['hp'] > 0) {
-                $character['hp'] -= $monster['ap'];
-                echo "The {$monster['name']} attacks! Character HP: {$character['hp']}";
-            }
+           // Monster attacks
+           if ($monster['hp'] > 0) {
+            $character['hp'] -= $monster['ap'];
+            echo "The {$monster['name']} attacks! Character HP: {$character['hp']}";
         }
-
-        // Check who died
-        if ($character['hp'] <= 0) {
-            echo "{$character['name']} is defeated by the {$monster['name']}!";
-            exit;
-        } else {
-            echo "{$character['name']} defeats the {$monster['name']}!";
-
-            // Add a bonus to all stats
-            $bonus = 10;
-            $character['hp'] += $bonus;
-            $character['ap'] += $bonus;
-            $character['dp'] += $bonus;
-
-            // Update the character's attributes in the database
-            $stmt = $this->connexion->prepare("UPDATE characters SET hp = ?, ap = ?, dp = ? WHERE id = ?");
-            $stmt->execute([$character['hp'], $character['ap'], $character['dp'], $this->character->id]);
-
-            // Display bonus message
-            echo "{$character['name']} gains a bonus of $bonus to HP, AP, and DP!";
-        }
-    } else {
-        echo "{$character['name']} finds no monsters to fight.";
     }
 
-    // Restore initial stats for the next combat
-    $character['hp'] = $initialStats['hp'];
-    $character['ap'] = $initialStats['ap'];
-    $character['dp'] = $initialStats['dp'];
-
-    // Update the character's attributes in the database
-    $stmt = $this->connexion->prepare("UPDATE characters SET hp = ?, ap = ?, dp = ? WHERE id = ?");
-    $stmt->execute([$character['hp'], $character['ap'], $character['dp'], $this->character->id]);
-
-    // Check if the character's HP is 0 or less
+    // Check who died
     if ($character['hp'] <= 0) {
-        echo "{$character['name']} has died.";
+        echo "{$character['name']} is defeated by the {$monster['name']}!";
         exit;
+    } else {
+        echo "{$character['name']} defeats the {$monster['name']}!";
+
+        // Add a bonus to all stats
+        $bonus = 10;
+        $character['hp'] += $bonus;
+        $character['ap'] += $bonus;
+        $character['dp'] += $bonus;
+
+        // Award experience points
+        $expGain = 20; // Adjust the amount of XP gained as needed
+        $character['experience'] += $expGain;
+
+        // Check for level-up
+        if ($character['experience'] >= 100) {
+            $character['level'] += 1;
+            $character['experience'] = 0; // Reset XP after level-up
+
+            // Adjust other character stats or provide additional bonuses for level-up
+            // For example, you could increase HP, AP, DP, etc.
+            $character['hp'] += 10;
+            $character['ap'] += 5;
+            $character['dp'] += 5;
+
+            echo "{$character['name']} has leveled up to level {$character['level']}!";
+        }
+
+        // Update the character's attributes in the database
+        $stmt = $this->connexion->prepare("UPDATE characters SET hp = ?, ap = ?, dp = ?, experience = ?, level = ? WHERE id = ?");
+        $stmt->execute([$character['hp'], $character['ap'], $character['dp'],$character['experience']
+        , $character['level'], $this->character->id]);
+
+        // Display bonus and XP message
+        echo "{$character['name']} gains a bonus of $bonus to HP, AP, and DP!";
+        echo "{$character['name']} gains $expGain experience points. Total XP: {$character['experience']}";
     }
+} else {
+    echo "{$character['name']} finds no monsters to fight.";
+}
+
+// Restore initial stats for the next combat
+$character['hp'] = $initialStats['hp'];
+$character['ap'] = $initialStats['ap'];
+$character['dp'] = $initialStats['dp'];
+$character['experience'] = $initialStats['experience'];
+
+// Update the character's attributes in the database
+$stmt = $this->connexion->prepare("UPDATE characters SET experience = ? WHERE id = ?");
+$stmt = $this->connexion->prepare("UPDATE characters SET hp = ?, ap = ?, dp = ?, experience = ? WHERE id = ?");
+// Check if the character's HP is 0 or less
+if ($character['hp'] <= 0) {
+    echo "{$character['name']} has died.";
+    exit;
+}
 }
 function merchant() {
     // Fetch character data
@@ -343,7 +366,6 @@ function merchant() {
     }
 }
 
-    
 function loot() {
     $characterId = $this->character->id; // Récupère l'ID du personnage
 
@@ -356,16 +378,16 @@ function loot() {
         return;
     }
 
-    // Logique pour générer un objet aléatoire après la victoire
+    // Logic to generate a random item after victory
     $stmt = $this->connexion->prepare("SELECT * FROM loots ORDER BY RAND() LIMIT 1");
     $stmt->execute();
     $randomItem = $stmt->fetch();
 
-    if ($character['hp'] > 0) {
+    if (isset($character['hp']) && $character['hp'] > 0) {
         if ($randomItem) {
             $magicalItem = $randomItem['magical_items'];
             $cursedItem = $randomItem['cursed_items'];
-    
+
             if ($magicalItem) {
                 echo "You found a new magical item: $magicalItem\n";
             } elseif ($cursedItem) {
@@ -374,16 +396,16 @@ function loot() {
             } else {
                 echo "You found an empty chest. No items found.\n";
             }
-    
-            // Vérification si le personnage possède déjà cet objet dans son inventaire
+
+            // Check if the character already has this item in their inventory
             if ($magicalItem || $cursedItem) {
                 $itemAlreadyInInventory = false;
-    
+
                 if (($magicalItem && strpos($character['inventaire'], $magicalItem) !== false) || ($cursedItem && strpos($character['inventaire'], $cursedItem) !== false)) {
                     $itemAlreadyInInventory = true;
                     echo "You already have this item in your inventory.\n";
                 }
-    
+
                 if (!$itemAlreadyInInventory) {
                     $choice = readline("Do you want to add this item to your inventory? (yes/no): ");
                     if (strtolower($choice) === 'yes') {
@@ -401,8 +423,8 @@ function loot() {
             echo "No item found.\n";
         }
     }
-    
 }
+
 
 
 
